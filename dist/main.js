a1631cf81b4f01841fa8c411527d6db2f7489e1c
Jolomi Dudu <jollofdudu@gmail.com>
2026-09-24 21:40:55 +0200

    mild update

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,7 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors({
-        origin: process.env.CORS_ORIGIN ?? "http://localhost:3000",
+        origin: process.env.CORS_ORIGIN ?? "https://festyvibe.vercel.app",
         credentials: true
     });
     app.setGlobalPrefix("api");
